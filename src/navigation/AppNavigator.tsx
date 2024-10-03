9d18792c50c725773c8dfc390489fd59d290fd6a
WanderingDreamer <gabi.aug.santos@gmail.com>
2024-10-03 20:27:23 -0300

    aula 26/09

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -22,12 +22,36 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
     return (
         <Stack.Navigator initialRouteName="Welcome">
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
-            <Stack.Screen name="ConsultationsList" component={ConsultationsListScreen} />
-            <Stack.Screen name="ScheduleConsultation" component={ScheduleConsultationScreen} />
-            <Stack.Screen name="ConfirmAppointment" component={ConfirmAppointmentScreen} />
+            <Stack.Screen
+                name="Welcome"
+                component={WelcomeScreen}
+                options={{ title: 'Tela Inicial' }} // Título personalizável
+            />
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ title: 'Acesse sua conta' }} // Título personalizável
+            />
+            <Stack.Screen
+                name="SignUp"
+                component={SignUpScreen}
+                options={{ title: 'Cadastrar' }} // Título personalizável
+            />
+            <Stack.Screen
+                name="ConsultationsList"
+                component={ConsultationsListScreen}
+                options={{ title: 'Consultas Agendadas' }} // Título personalizável
+            />
+            <Stack.Screen
+                name="ScheduleConsultation"
+                component={ScheduleConsultationScreen}
+                options={{ title: 'Agendar Consulta' }} // Título personalizável
+            />
+            <Stack.Screen
+                name="ConfirmAppointment"
+                component={ConfirmAppointmentScreen}
+                options={{ title: 'Confirmação de Agendamento' }} // Título personalizável
+            />
         </Stack.Navigator>
     );
 };
